72d6383c0663718d34c8fbd0ef43e39587a37882
TDAT-PC\User <tdat5415@gmail.com>
2024-06-02 18:51:29 +0900

    init

--- server.py
+++ server.py
@@ -1,59 +1,15 @@
-from langchain_core.messages import ChatMessage
-
 import streamlit as st
-import time
 
-aaa = 123
+st.set_page_config(page_title="Page Title ")
+st.title('Title : Streamlit Test')
 
-st.set_page_config(page_title="Page Title123")
+def response(input_text):
+    """
+    # 입력받은 input text 를 그대로 출력
+    """
+    st.info(input_text)
 
-@st.cache_resource(show_spinner="loading...")
-def init():
-    global aaa
-    time.sleep(3)
-    aaa = 1234
-
-init()
-
-st.info(aaa)
-
-
-st.title('Title123')
-# with st.form('Question123'):
-#     text = st.text_area('질문 내용:', '질문 기본값1234') #첫 페이지가 실행될 때 보여줄 질문
-#     submitted = st.form_submit_button('보내기') # 버튼 이름
-#     st.info(text) # 텍스트 상자
-
-############################ 파일 업로드
-@st.cache_resource(show_spinner="Embedding file...")
-def embed_file(file):    
-    file_content = file.read()
-    st.info(file.name)
-    st.info(file)
-    time.sleep(1)
-
-with st.sidebar:
-    file = st.file_uploader("파일 업로드", type=["pdf", "txt", ], )
-
-if file:
-    embed_file(file)
-    
-
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        ChatMessage(role="assistant", content="무엇을 도와드릴까요?")
-    ]
-
-for msg in st.session_state.messages:
-    st.chat_message(msg.role).write(msg.content)
-
-user_input = st.chat_input()
-
-st.info(user_input)
-
-
-
-
-
-
-
+with st.form('Question'):
+    text = st.text_area('질문 내용:', '질문 기본값') #첫 페이지가 실행될 때 보여줄 질문
+    submitted = st.form_submit_button('보내기') # 버튼 이름
+    response(text)
